3e3ebee89122248946fccbefba0724fff1756a63
BogdanKandra <bogdan.kandra@gmail.com>
2020-12-21 00:37:25 +0200

    Updated model file path

--- src/create_model.py
+++ src/create_model.py
@@ -3,13 +3,17 @@ from tensorflow.keras.applications import vgg16
 
 
 # Constants
-MODEL_PATH = 'model.pkl'
+if os.path.basename(os.getcwd()) == 'src':
+    MODEL_PATH = os.path.join(os.path.dirname(os.getcwd()), 'model.h5')
+else:
+    MODEL_PATH = 'model.h5'
 
 def create_model():
     ''' Loads the VGG16 network from Keras '''
     vgg16_model = vgg16.VGG16()
 
     return vgg16_model
+
 
 if __name__ == '__main__':
     # If model does not exist, create it and serialize it
