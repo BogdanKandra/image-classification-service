9823ac043d25858e04e30de4c30fcf294e5f6a20
BogdanKandra <bogdan.kandra@gmail.com>
2020-12-30 09:55:03 +0200

    Added better path management

--- src/create_model.py
+++ src/create_model.py
@@ -3,11 +3,14 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from tensorflow.keras.applications import vgg16
 
 
+# Setting the project directory path, for easy access to resources
+project_directory_path = os.getcwd()
+while os.path.basename(project_directory_path) not in ['app', 'image-classification-service']:
+    project_directory_path = os.path.dirname(project_directory_path)
+
 # Constants
-if os.path.basename(os.getcwd()) == 'src':
-    MODEL_PATH = os.path.join(os.path.dirname(os.getcwd()), 'model.h5')
-else:
-    MODEL_PATH = 'model.h5'
+MODEL_PATH = os.path.join(project_directory_path, 'model.h5')
+
 
 def create_model():
     ''' Loads the VGG16 network from Keras '''
@@ -20,6 +23,6 @@ if __name__ == '__main__':
     # If model does not exist, create it and serialize it
     if not os.path.exists(MODEL_PATH):
         model = create_model()
-        model.save('model.h5')
+        model.save(MODEL_PATH)
     else:
         print('>>> Model already exists!')
